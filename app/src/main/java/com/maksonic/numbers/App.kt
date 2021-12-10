fa6ec54e25e78eddf28e82bd812bf81adf4678c8
maksonic <maksonic.official@gmail.com>
2021-12-10 17:11:57 +0300

    Init support emoji. Updated moduleList

--- app/src/main/java/com/maksonic/numbers/App.kt
+++ app/src/main/java/com/maksonic/numbers/App.kt
@@ -1,9 +1,12 @@
 package com.maksonic.numbers
 
 import android.app.Application
+import androidx.emoji2.bundled.BundledEmojiCompatConfig
+import androidx.emoji2.text.EmojiCompat
 import com.maksonic.numbers.core.coreModule
 import com.maksonic.numbers.data.dataModule
 import com.maksonic.numbers.domain.domainModule
+import com.maksonic.numbers.presentation.main_screen.di.mainScreenModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -12,7 +15,7 @@ import org.koin.core.context.startKoin
  */
 class App : Application() {
 
-    private val moduleList = listOf(coreModule, domainModule, dataModule)
+    private val moduleList = listOf(coreModule, domainModule, dataModule, mainScreenModule)
 
     override fun onCreate() {
         super.onCreate()
@@ -20,5 +23,11 @@ class App : Application() {
             androidContext(this@App)
             modules(moduleList)
         }
+        initEmoji()
+    }
+
+    private fun initEmoji() {
+        val config = BundledEmojiCompatConfig(this)
+        EmojiCompat.init(config)
     }
 }
